f21672e4b02170a98dd3fd315d7ce29c8e59d6a2
hhh <957400829@qq.com>
2018-03-28 21:00:52 +0800

    Initial commit

---
+++ src/main/java/com/iot/DiscardServerHandler.java
@@ -0,0 +1,32 @@
+package com.iot;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * Created by xiongxiaoyu
+ * Data:2018/3/28
+ * Time:20:58
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+	public void channelRead(ChannelHandlerContext ctx, Object msg) {
+		ByteBuf in = (ByteBuf) msg;
+		try {
+			while (in.isReadable()) { // (1)
+				System.out.print((char) in.readByte());
+				System.out.flush();
+			}
+		} finally {
+			ReferenceCountUtil.release(msg); // (2)
+		}
+	}
+
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)throws Exception  { // (4)
+		// Close the connection when an exception is raised.
+		cause.printStackTrace();
+		ctx.close();
+	}
+}
